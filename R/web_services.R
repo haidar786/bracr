6cf48cbdc24f304424faa6536b9ed784e6a71056
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-27 16:39:28 +0200

    server time, symbolInfo, orderbook, trades functions added

--- R/web_services.R
+++ R/web_services.R
@@ -4,14 +4,64 @@ library(future)
 
 baseApi <- "https://api.binance.com"
 
-pingToBinanceApi <- function() {
+
+pingToServer <- function() {
   endPoint <- "/api/v3/ping"
   getFutureRes <- future(GET(url = paste0(baseApi, endPoint)))
   resValue <- value(getFutureRes)
-  status_code(resValue)
+  sc <- status_code(resValue)
+  if (sc == 1) {
+
+  }
   content <- content(resValue, "text")
   return(as.data.frame(content))
 }
-pingToBinanceApi()
+
+pingToServer()
+
+checkServerTime <- function() {
+  endPoint <- "/api/v3/time"
+  getFutureRes <- future(GET(url = paste0(baseApi, endPoint)))
+  resValue <- value(getFutureRes)
+  sc <- status_code(resValue)
+  if (sc == 1) {
+
+  }
+  content <- content(resValue, "text")
+  return(as.data.frame(content))
+}
+
+checkServerTime()
+
+fetchSymbolExchangeInfo <- function(symbol) {
+  endPoint <- paste0("/api/v3/exchangeInfo?symbol=", symbol)
+  getFutureRes <- future(GET(url = paste0(baseApi, endPoint)))
+  resValue <- value(getFutureRes)
+  content <- content(resValue, "text")
+  return(as.data.frame(content))
+}
+
+fetchSymbolExchangeInfo(symbol = "BNBBTC")
+
+fetchSymbolOrderBook <- function(symbol) {
+  endPoint <- paste0("/api/v3/depth?symbol=", symbol)
+  getFutureRes <- future(GET(url = paste0(baseApi, endPoint)))
+  resValue <- value(getFutureRes)
+  content <- content(resValue, "text")
+  return(as.data.frame(content))
+}
+
+fetchSymbolOrderBook(symbol = "BNBBTC")
+
+fetchSymbolTrades <- function(symbol) {
+  endPoint <- paste0("/api/v3/trades?symbol=", symbol)
+  getFutureRes <- future(GET(url = paste0(baseApi, endPoint)))
+  resValue <- value(getFutureRes)
+  content <- content(resValue, "text")
+  return(as.data.frame(content))
+}
+
+fetchSymbolTrades(symbol = "BNBBTC")
+
 
 
